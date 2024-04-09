3d1e26c3fdce2b94d9b20e9fe1637e77887096c4
Egeeoz <ozakdemirege@hotmail.com>
2024-04-09 20:20:37 +0200

    Created the login endpoint to only allow either username or email, not both, to authenticate a user. This prevents login with mismatched email/username pairs and enforces a more secure and predictable login process

--- index.js
+++ index.js
@@ -32,20 +32,24 @@ app.get("/api/menu", async (req, res) => {
 });
 
 app.post("/api/signup", async (req, res) => {
+  // deconstructing
   const { userName, email, password } = req.body;
 
   try {
+    // Looking for user with email and username
     const existingUser = await db.findOne({ $or: [{ userName }, { email }] });
+    // If user with thoose credentials exists then send message that account already exists
     if (existingUser) {
       res.status(409).json("Account already exists");
     } else {
+      // Otherwise take the given info and put them into the new user
       const newUser = {
         userName,
         email,
         password,
         orders: [],
       };
-
+      // Send the new user info into the database and return the created account
       const user = await db.insert(newUser);
       res.status(201).json(user);
     }
@@ -54,6 +58,42 @@ app.post("/api/signup", async (req, res) => {
   }
 });
 
+app.post("/api/login", async (req, res) => {
+  // deconstructing
+  const { userName, email, password } = req.body;
+
+  let userCredential;
+  // If user tries to log in with both email and username send message to only try to login with email OR username
+  if (userName && email) {
+    return res.status(400).send("Please provide a userName or email");
+  }
+
+  // User logins with username
+  if (userName) {
+    userCredential = { userName };
+  } else if (email) {
+    // User logins with email
+    userCredential = { email };
+  } else {
+    // If no username or email is provided, return message
+    return res.status(400).send("Please provide a userName or email");
+  }
+
+  try {
+    // Find the user with the given credential and save it into an variable
+    const user = await db.findOne(userCredential);
+    // Check if user password match given user credential
+    if (user && user.password == password) {
+      // If it matches, log in
+      res.status(200).json({ message: "Login Successful", user });
+    } else {
+      // If it does not match, return message (no login)
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (error) {
+    res.status(500).send("Internal server error");
+  }
+});
 //user
 //user/orderHistory
 
